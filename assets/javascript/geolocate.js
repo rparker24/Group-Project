7fd84727d2e1ab4aa4ca40676a59988fa62bfdc4
kavarnold02 <kevarnold02@gmail.com>
2016-03-26 14:28:39 -0400

    geolocate.js file updated

--- assets/javascript/geolocate.js
+++ assets/javascript/geolocate.js
@@ -24,7 +24,30 @@ function success(pos) {
     map: map,
     title: 'Hello World!'
   });
+
+  var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
+
 };
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
 
 function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
