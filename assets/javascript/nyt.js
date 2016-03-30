bc83adab668bdcf5612e21d0be5245eb3720d9a6
kavarnold02 <kevarnold02@gmail.com>
2016-03-30 18:34:12 -0400

    added secure https to the link in nyt.js file

--- assets/javascript/nyt.js
+++ assets/javascript/nyt.js
@@ -1,6 +1,6 @@
 $('#submit').on('click', function() {    
 	var city = $('#address').val();
-	var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=glocations%3A%28%22" + city + "%22%29&begin_date=19000101&sort=oldest&fl=headline%2Csnippet%2Cpub_date%2Ckeywords&api-key=9d4a8986921972b65754ea0809d47c84%3A12%3A74623931";
+	var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=glocations%3A%28%22" + city + "%22%29&begin_date=19000101&sort=oldest&fl=headline%2Csnippet%2Cpub_date%2Ckeywords&api-key=9d4a8986921972b65754ea0809d47c84%3A12%3A74623931";
         $.ajax({
                 url: queryURL,
                 method: 'GET'
